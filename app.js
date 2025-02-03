755293a5a699189de811cd12d31548d7cc454bd6
José Romildo <pronuno1@gmail.com>
2025-02-03 12:20:17 -0300

    Implementa função para ler entrada do usuário

---
+++ app.js
@@ -0,0 +1,18 @@
+let amigos = [];
+
+function adicionarAmigo() {
+    let entrada = lerEntrada();
+}
+
+function sortearAmigo() {
+
+}
+
+//----------------- Funções Auxiliares -----------------
+
+function lerEntrada() {
+    const entrada = document.querySelector('input');
+    let nome = entrada.value.trim();
+    return nome;
+}
+
